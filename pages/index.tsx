099b66f8dc9eeca98d6d5c49ba495cb62eb12012
Ahmed Ali <imahmedalibhatti@gmail.com>
2023-03-03 00:09:19 +0500

    send tokens

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,15 +7,58 @@ import {
   Flex,
   Icon,
   useColorMode,
+  Input,
 } from "@chakra-ui/react";
 import { BsFillMoonStarsFill, BsFillSunFill } from "react-icons/bs";
 import { WalletSection } from "../components";
-import { cw20ContractAddress } from "../config";
+import { chainName, cw20ContractAddress } from "../config";
 import { useTokenBalance } from "../hooks/useTokenBalance";
+import { useState } from "react";
+import { useChain } from "@cosmos-kit/react";
+import { Cw20Client } from "../codegen/Cw20.client";
 
 export default function Home() {
   const { colorMode, toggleColorMode } = useColorMode();
   const balance = useTokenBalance(cw20ContractAddress);
+  const [amount, setAmount] = useState<string | undefined>(undefined);
+  const [recipient, setRecipient] = useState<string | undefined>(undefined);
+  const [txHash, setTxHash] = useState<string | undefined>(undefined);
+
+  const { getSigningCosmWasmClient, address } = useChain(chainName);
+
+  const handleRecipient = (e: any) => {
+    setRecipient(e.target.value);
+  };
+
+  const handleAmount = (e: any) => {
+    setAmount(e.target.value);
+  };
+
+  const handleSend = async () => {
+    const cosmWasmClient = await getSigningCosmWasmClient();
+
+    if (!cosmWasmClient || !address) {
+      console.error("No cosmWasmClient or No address!");
+      return;
+    }
+
+    const client = new Cw20Client(cosmWasmClient, address, cw20ContractAddress);
+
+    if (!client) {
+      console.error("no client, please connect your wallet!");
+      return;
+    }
+
+    if (!amount || !recipient) {
+      console.error("No amount or recipient!");
+      return;
+    }
+
+    const result = await client.transfer({ amount, recipient });
+    console.log(result);
+
+    setTxHash(result.transactionHash);
+  };
 
   return (
     <Container maxW="5xl" py={10}>
@@ -44,6 +87,14 @@ export default function Home() {
       <WalletSection />
       <Box textAlign="center">
         <p>Your token balance is: {balance}</p>
+
+        <Input
+          placeholder="Address"
+          value={recipient}
+          onChange={handleRecipient}
+        />
+        <Input placeholder="Amount" value={amount} onChange={handleAmount} />
+        <Button onClick={handleSend}>Send!</Button>
       </Box>
     </Container>
   );
